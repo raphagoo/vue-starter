3de1766978253c6c223bc8c05c3711f768f0ea9f
Pascal Sancho <psancho.13@gmail.com>
2019-03-05 16:22:52 +0100

    NEW added various config files

---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const path = require(`path`);
+
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const env = process.env.NODE_ENV || 'development';
+console.log(env);
+
+module.exports = {
+    mode: env,
+    entry: path.join(__dirname, `src`, `main.js`),
+    output: {
+        publicPath: `/`,
+    },
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.(js|vue)$/,
+                loader: 'eslint-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+            },
+            // this will apply to both plain `.js` files
+            // AND `<script>` blocks in `.vue` files
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: file => (
+                    /node_modules/.test(file) &&
+                    !/\.vue\.js/.test(file)
+                )
+            },
+            // this will apply to both plain `.css` files
+            // AND `<style>` blocks in `.vue` files
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                ],
+            },
+            // this will apply to both plain `.scss` files
+            // AND `<style lang="scss">` blocks in `.vue` files
+            {
+                test: /\.scss$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        // options: {
+                        //     // you can also read from a file, e.g. `variables.scss`
+                        //     data: `$color: red;`
+                        // }
+                    }
+                ]
+            },
+        ]
+    },
+    plugins: [
+        // make sure to include the plugin!
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+            favicon: 'public/favicon.ico',
+            // filename: path.join(__dirname, `dist`, `index.html`),
+            inject: true,
+            // minify: minify ? {
+            //     removeComments: true,
+            //     collapseWhitespace: true,
+            //     removeAttributeQuotes: true,
+            //     // More options:
+            //     // https://github.com/kangax/html-minifier#options-quick-reference
+            // } : false,
+        }),
+    ]
+};
