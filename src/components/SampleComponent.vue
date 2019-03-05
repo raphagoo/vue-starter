3e79867a865393666b6f5977883f508edfa320a6
Pascal Sancho <psancho.13@gmail.com>
2019-03-05 16:22:12 +0100

    NEW initial project structure

---
+++ src/components/SampleComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <p>{{ greeting }} World!</p>
+    <!-- <LocalComponant1></LocalComponant1> -->
+</template>
+
+<script>
+// import LocalComponant1 from './localComponent1.vue';
+
+export default {
+    name: 'SampleComponent',
+    data: () => ({
+        greeting: 'Hello'
+    }),
+    // components: {
+    //     LocalComponant1,
+    // }
+}
+</script>
+
+<style lang="scss" scoped>
+p {
+    margin: 0;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <SampleComponent/>
+  </div>
+</template>
+
+<script>
+import SampleComponent from '../components/SampleComponent.vue'
+
+export default {
+    name: 'home',
+    components: {
+        SampleComponent
+    }
+}
+</script>
